c8ca38fd993b773c303a447f6e0c83c530265cc3
zhangyang <zyayhj.yhj@163.com>
2017-11-01 17:54:18 +0800

    添加StatusController,供外部获取实时状态数据

---
+++ src/main/java/com/bi7/bitch/controller/StatusController.java
@@ -0,0 +1,22 @@
+package com.bi7.bitch.controller;
+
+import com.bi7.bitch.conf.GethConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by foxer on 2017/11/1.
+ */
+@RestController
+@RequestMapping("/coin")
+public class StatusController {
+
+    @Autowired
+    GethConfig gethConfig;
+
+    @RequestMapping("/eth-scan-blocknumber")
+    public String ethBlockStatus() {
+        return String.valueOf(gethConfig.getStartBlockNumber());
+    }
+}
