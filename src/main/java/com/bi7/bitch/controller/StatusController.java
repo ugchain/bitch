05f28582b9605e1ccb063fc8a03b55a7c24e4336
zhangyang <zyayhj.yhj@163.com>
2017-11-01 18:43:30 +0800

    StatusControll mapping /status

--- src/main/java/com/bi7/bitch/controller/StatusController.java
+++ src/main/java/com/bi7/bitch/controller/StatusController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by foxer on 2017/11/1.
  */
 @RestController
-@RequestMapping("/coin")
+@RequestMapping("/status")
 public class StatusController {
 
     @Autowired
